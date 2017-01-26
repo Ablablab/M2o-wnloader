76c9bc3e9c14537b9a4097e1adae8efcf2e51e52
Ablablab <andrea.gennusa@gmail.com>
2017-01-26 22:01:13 +0100

    episode is now a class

--- downloader.py
+++ downloader.py
@@ -4,8 +4,17 @@ def get_page(url):
     page = html.fromstring(page_string)
     return page_string, page
 
+class Episode(object):
+    def __init__(self, name, page, link=""):
+        self.name = name
+        self.page = page
+        self.link = link
+
+    def __str__(self):
+        return str(self.name) + " -- " + str(self.link)
 
 url = 'http://www.m2o.it/special/real-trust-reloaded/'
+url_reloaded = "http://www.m2o.it/reloaded/"
 print "Mumble mumble... searching for Real Trust on " + url
 
 # this tool is for web requests
@@ -29,7 +38,14 @@ first_player_page_string, first_player_page = get_page(iframe_link)
 
 # now i find the real link list
 list_html = first_player_page.get_element_by_id("scrollbar1").find_class("overview")
-player_link_list = []
+episode_list = []
+
 for link in list_html[0].xpath("//a"):
-    player_link_list.append((link.text, link.get("href")))
-print "I found " + str(len(player_link_list)) + " tracks"
+    episode_list.append(Episode(link.text, link.get("href")))
+print "I found " + str(len(episode_list)) + " tracks"
+
+track_string, track_page = get_page(url_reloaded + episode_list[0].page)
+print track_string
+
+music_link = track_page.cssselect("audio")[0].attrib['src']
+print music_link
