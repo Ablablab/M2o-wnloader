1ea47c4abee58c051c47b0c4ee46c64706253bae
Ablablab <andrea.gennusa@gmail.com>
2017-01-26 21:30:07 +0100

    now it opens m2oreloaded and then get m2o.download url

--- downloader.py
+++ downloader.py
@@ -5,8 +5,23 @@ print "Mumble mumble... searching for Real Trust on " + url
 import urllib
 from lxml import html
 
+page_string = urllib.urlopen(url).read()
+page = html.fromstring(page_string)
 
-page = html.fromstring(urllib.urlopen(url).read())
+#for link in page.xpath("//a"):
+#    print "Name", link.text, "URL", link.get("href")
 
-for link in page.xpath("//a"):
+#print page_string
+
+iframe_link = page.cssselect("iframe")[0].attrib['src']
+print iframe_link
+
+first_player_page_string = urllib.urlopen(iframe_link).read()
+first_player_page = html.fromstring(first_player_page_string)
+
+print first_player_page_string
+print 1 + "a"
+
+list_html = page.get_element_by_id("scrollbar1").find_class("overview")
+for link in list_html.xpath("//a"):
     print "Name", link.text, "URL", link.get("href")
