0fd134c65ddd7e49c240382a1bab304e5773e12c
Ablablab <andrea.gennusa@gmail.com>
2017-01-26 20:30:22 +0100

    using urllib

--- downloader.py
+++ downloader.py
@@ -1,12 +1,12 @@
-print "Mumble mumble... searching for Real Trust"
-import urllib
-
-import re
-pat = re.compile('<DT><a href="[^"]+">(.+?)</a>')
-
 url = 'http://www.m2o.it/special/real-trust-reloaded/'
-sock = urllib.urlopen(url)
-li = pat.findall(sock.read())
-sock.close()
+print "Mumble mumble... searching for Real Trust on " + url
 
-print li
+
+import urllib
+from lxml import html
+
+
+page = html.fromstring(urllib.urlopen(url).read())
+
+for link in page.xpath("//a"):
+    print "Name", link.text, "URL", link.get("href")
